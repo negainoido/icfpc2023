87fa6525a2eb422c9c84048ada56a47fb6efa45d
garasubo <garasubo@gmail.com>
2023-06-18 22:13:50 +0900

    add sveltekit skelton

---
+++ webapp/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { onMount } from 'svelte';
+	onMount(async () => {
+		const wasm = await import('wasm-sample');
+		await wasm.default();
+		console.log(wasm.add(1, 2));
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
