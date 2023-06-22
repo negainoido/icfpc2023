49faf51baa38264af44d8d5ab6e36eefc121a59e
Takumi Shimada <garasubo@gmail.com>
2023-06-22 22:53:22 +0900

    add github action (#2)
    * add github action
    
    * add deploy script
    
    * add personal github token

--- webapp/svelte/src/routes/+page.svelte
+++ webapp/svelte/src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { onMount } from 'svelte';
-	onMount(async () => {
-		const wasm = await import('wasm-sample');
-		await wasm.default();
-		console.log(wasm.add(1, 2));
-	});
+    import { onMount } from 'svelte';
+    onMount(async () => {
+        const wasm = await import('wasm-sample');
+        await wasm.default();
+        console.log(wasm.add(1, 2));
+    });
 </script>
 
 <h1>Welcome to SvelteKit</h1>
