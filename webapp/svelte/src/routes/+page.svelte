fe2538b37801b513ba7317422ef27678a217e94b
cympfh <cympfh@gmail.com>
2023-07-02 13:30:25 +0900

    wasm-addition

--- webapp/svelte/src/routes/+page.svelte
+++ webapp/svelte/src/routes/+page.svelte
@@ -1,11 +1,27 @@
 <script>
     import { onMount } from 'svelte';
+    let wasm;
+    let x = 1;
+    let y = 2;
+    let z;
+
     onMount(async () => {
-        const wasm = await import('wasm-sample');
+        wasm = await import('wasm-sample');
         await wasm.default();
-        console.log(wasm.add(1, 2));
+        z = wasm.add(x, y);
     });
+
+    function updateAddition() {
+        if (!wasm) return; // failed
+        z = wasm.add(x, y);
+    }
+
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <input type="number" bind:value={x} on:change={updateAddition} /> +
+    <input type="number" bind:value={y} on:change={updateAddition} /> =
+    <input type="number" bind:value={z} disabled />
+</div>
