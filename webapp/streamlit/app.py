58dd2d92a1c6398f3b60ab1e024dd306cbd05bad
/c/ympfh <cympfh@gmail.com>
2023-07-08 03:42:55 +0900

    今あるAPIを使える (#52)

--- webapp/streamlit/app.py
+++ webapp/streamlit/app.py
@@ -1,85 +1,63 @@
-import json
-import os
+from io import StringIO
 
 import pandas
 import requests
-import streamlit
 
-
-def read_secrets():
-    if SECRET := os.environ.get("SECRET"):
-        return json.loads(SECRET)
-    return streamlit.secrets
-
-
-secrets = read_secrets()
+import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 
 class API:
     def __init__(self):
-        self.endpoint = secrets["backend"]
+        self.url = "https://icfpc2023.negainoido.com"
 
-    def hello(self):
-        url = f"{self.endpoint}/hello"
-        res = requests.get(url)
-        return res.json()
-
-    def show(self):
-        url = f"{self.endpoint}/scores/show"
-        res = requests.get(url)
-        return res.json()
-
-    def append(self, name: str, score: int, auth: str):
-        url = f"{self.endpoint}/scores/append"
-        data = {
-            "name": name,
-            "score": score,
-        }
+    def _get(self, endpoint: str, data=None):
         headers = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {auth}",
         }
-        res = requests.post(url, json=data, headers=headers)
-        return res.json()
+        response = requests.get(f"{self.url}{endpoint}", data=data, headers=headers)
+        return response.json()
 
+    def show(self):
+        data = self._get("/api/solutions/show")
+        return data
 
-streamlit.write("# Hello, World!")
+    def submit(self, problem_id: int, solver: str, uploaded_file: UploadedFile):
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        response = requests.post(
+            f"{self.url}/api/solutions/submit?id={problem_id}&solver={solver}",
+            files={"file": stringio},
+        )
+        return response.json()
 
 
 api = API()
-streamlit.write(api.hello())
-
-# score dashboard
-if streamlit.button(":arrows_counterclockwise: refresh"):
-    streamlit.experimental_rerun()
+if st.button(":arrows_counterclockwise: refresh"):
+    st.experimental_rerun()
 
 rows = api.show()
-streamlit.write(f"{len(rows)} records")
-streamlit.dataframe(
-    pandas.DataFrame(rows, columns=["id", "name", "score", "timestamp"]),
+
+st.write("## Submission")
+st.write(f"{len(rows)} records")
+st.dataframe(
+    pandas.DataFrame(
+        rows,
+        columns=["id", "problem_id", "submittion_id", "solver", "score", "timestamp"],
+    ),
     hide_index=True,
     column_config={
-        "score": streamlit.column_config.ProgressColumn(
+        "score": st.column_config.ProgressColumn(
             "score",
             format="%d",
             min_value=0,
-            max_value=1000,
         ),
     },
 )
 
-# submit new record
-streamlit.write("## submit new record")
-file = streamlit.file_uploader("your file (dummy)")  # dummy
-name = streamlit.text_input("name", str(file.name) if file else "")
-score = int(
-    streamlit.number_input("score", min_value=0, max_value=1000, step=1, value=0)
-)
-if streamlit.button("Submit"):
-    if not name or not score:
-        streamlit.warning("name and positive score are required")
-        streamlit.stop()
-    res = api.append(name, score, auth=secrets["auth"])
-    streamlit.json(res)
-    if res.get("status") == "OK":
-        streamlit.balloons()
+st.write("## Submit new file")
+problem_id = int(st.number_input("problem_id", value=1, min_value=1, max_value=45))
+solver = st.text_input("solver name", value="default")
+jsonfile = st.file_uploader("JSON File")
+if problem_id and solver and jsonfile:
+    if st.button("submit"):
+        st.json(api.submit(problem_id, solver, jsonfile))
