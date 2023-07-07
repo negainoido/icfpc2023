a7b02059f9f5b36705283d4b6af2de761179c3b3
Takuto Ikuta <atetubou@gmail.com>
2023-07-07 23:18:14 +0900

    defautl

--- solver/src/bin/random.rs
+++ solver/src/bin/random.rs
@@ -18,7 +18,18 @@ fn main() {
     let input_str = std::fs::read_to_string(args.input).unwrap();
     let input: Input = serde_json::from_str(&input_str).unwrap();
 
+    let mut solution: Solution = Default::default();
+    let mut cx = input.stage_bottom_left.x + 10.0;
+    let mut cy = input.stage_bottom_left.y + 10.0;
 
+    for _i in 0..input.musicians.len() {
+        solution.placements.push(Pos { x: cx, y: cy });
+        cx += 20.0;
+        if cx + 10.0 > input.stage_bottom_left.x + input.stage_height {
+            cx = input.stage_bottom_left.x + 10.0;
+            cy += 20.0;
+        }
+    }
 
     println!("Input: {:?}", input);
 }
