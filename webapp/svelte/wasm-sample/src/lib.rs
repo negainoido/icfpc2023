c4fab6cbe4cfa8dc645d0be726d2ee06412681d5
autotaker <autotaker@gmail.com>
2023-07-08 05:35:47 +0900

    add rust-cache (#56)
    * add rust-cache
    
    * add workspace config
    
    * modify lib.rs to test cache

--- webapp/svelte/wasm-sample/src/lib.rs
+++ webapp/svelte/wasm-sample/src/lib.rs
@@ -4,3 +4,8 @@ use wasm_bindgen::prelude::*;
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+#[wasm_bindgen]
+pub fn sub(left: usize, right: usize) -> usize {
+    left - right
+}
