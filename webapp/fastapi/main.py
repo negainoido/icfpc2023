4bad50bc55f1e801d2f9d2fa0e3fc9c1d5aa6962
/c/ympfh <cympfh@gmail.com>
2023-07-08 01:38:42 +0900

    :art: refactor (#41)
    * :art: refactor
    
    * use json
    
    * use json

--- webapp/fastapi/main.py
+++ webapp/fastapi/main.py
@@ -1,32 +1,49 @@
 import json
-import requests
 import os
-import secrets as sec
 
 import pymysql
-from fastapi import FastAPI, HTTPException, UploadFile
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from pydantic import BaseModel
-from google.cloud.sql.connector import Connector
+import requests
 from google.cloud import storage
+from google.cloud.sql.connector import Connector
+from pydantic import BaseModel
+
+from fastapi import FastAPI, UploadFile
 
 secrets = json.loads(os.environ.get("SECRET", ""))
 app = FastAPI()
 connector = Connector()
 storage_client = storage.Client()
 
+
 def get_icfpc_token():
-    data={ "username_or_email": secrets["icfpc"]["username"], "password": secrets["icfpc"]["password"] }
-    resp = requests.post("https://api.icfpcontest.com/login", data=json.dumps(data), headers={"Content-Type": "application/json"})
+    data = {
+        "username_or_email": secrets["icfpc"]["username"],
+        "password": secrets["icfpc"]["password"],
+    }
+    resp = requests.post(
+        "https://api.icfpcontest.com/login",
+        json=data,
+        headers={"Content-Type": "application/json"},
+    )
     json_resp = resp.json()
     return json_resp["Success"]
 
+
 icfpc_token = get_icfpc_token()
 
+
 def submit_solution_to_icfpc(problem_id: int, contents: str):
-    data = { "problem_id": problem_id, "contents": contents }
-    resp = requests.post("https://api.icfpcontest.com/submission", data=json.dumps(data), headers={"Content-Type": "application/json", "Authorization": f"Bearer {icfpc_token}"})
+    data = {"problem_id": problem_id, "contents": contents}
+    resp = requests.post(
+        "https://api.icfpcontest.com/submission",
+        json=data,
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {icfpc_token}",
+        },
+    )
     return resp.text
+
 
 class Scores:
     def __init__(self):
@@ -52,7 +69,7 @@ class Scores:
                 "pymysql",
                 user=self.user,
                 password=self.password,
-                db=self.database
+                db=self.database,
             )
         elif self.con_type == "tcp":
             return pymysql.connect(
@@ -63,7 +80,7 @@ class Scores:
                 database=self.database,
             )
         else:
-            raise Exception('invalid db_type:', self.db_type)
+            raise Exception("invalid con_type:", self.con_type)
 
     def _init_table(self):
         sql = """
@@ -105,13 +122,13 @@ class Scores:
         with self.con() as con:
             with con.cursor() as cur:
                 cur.execute(sql, (solver, problem_id, submission_id))
-                rows = cur.fetchall()  # type: ignore
+                _ = cur.fetchall()  # type: ignore
                 id = cur.lastrowid
             con.commit()
         bucket = storage_client.bucket(self.bucket_name)
-        blob = bucket.blob(f'{self.blob_prefix}/{id}.json')
-        with blob.open('w') as f:
-            f.write(content)
+        blob = bucket.blob(f"{self.blob_prefix}/{id}.json")
+        with blob.open("w") as f:
+            f.write(content)  # type: ignore
 
 
 scores = Scores()
