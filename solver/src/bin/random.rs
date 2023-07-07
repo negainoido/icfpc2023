9ed246d3570da662fc174373872aa7a9ce5e6320
Takuto Ikuta <atetubou@gmail.com>
2023-07-07 22:21:17 +0900

    output

--- solver/src/bin/random.rs
+++ solver/src/bin/random.rs
@@ -8,12 +8,17 @@ struct Args {
     /// Name of the person to greet
     #[arg(short, long)]
     input: String,
+
+    #[arg(short, long)]
+    output: String,
 }
 
 fn main() {
     let args = Args::parse();
     let input_str = std::fs::read_to_string(args.input).unwrap();
     let input: Input = serde_json::from_str(&input_str).unwrap();
+
+
 
     println!("Input: {:?}", input);
 }
