99961600db52d80e638f9ecf16046a03bd92cb20
garasubo <garasubo@gmail.com>
2023-07-08 00:35:09 +0900

    fix api

--- tools/submit.sh
+++ tools/submit.sh
@@ -5,8 +5,10 @@ set -euxo pipefail
 # Usage: set $PROBLEM_ID, $ANSWER_FILE, and $SOLVER (optional)
 # Example: PROBLEM_ID=1 ANSWER_FILE=./test.json SOLVER=test ./tools/submission.sh
 # 自分たちのサーバーの提出用エンドポイントを叩きます。ICFPC2023のサーバーにも提出されます。
+# `gcloud auth login` しておく必要があります
 
 SOLVER=${SOLVER:-default}
 ANSWER_FILE=$(readlink -f "$ANSWER_FILE")
 
-curl -X POST -F file=@${ANSWER_FILE} "https://icfpc2023.negainoido.com/api/solutions/submit?id=${PROBLEM_ID}&solver=${SOLVER}"
+TOKEN=$(gcloud auth print-access-token)
+curl -X POST -H "Authorization: Bearer ${TOKEN}" -F file=@"${ANSWER_FILE}" "https://icfpc2023.negainoido.com/api/solutions/submit?id=${PROBLEM_ID}&solver=${SOLVER}"
