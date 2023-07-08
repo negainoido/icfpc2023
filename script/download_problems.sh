5cfab62b3f8bad506369031bf495592e49c726b8
/c/ympfh <cympfh@gmail.com>
2023-07-08 11:15:18 +0900

    update problems (#68)

--- script/download_problems.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 1 55); do
-  curl -L http://api.icfpcontest.com/problem?problem_id=$i |
-    jq -r '.Success' |
-    jq -c . > /tmp/json
-  cp /tmp/json problems/problem-$i.json
-  cp /tmp/json webapp/streamlit/resource/problems/problem-$i.json
-done
