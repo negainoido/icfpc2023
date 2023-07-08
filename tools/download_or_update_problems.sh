abf8f07a2f960681a246e24bbc788a07b4c64b3c
/c/ympfh <cympfh@gmail.com>
2023-07-08 17:43:34 +0900

    fastapi: /api/problem (#103)

--- tools/download_or_update_problems.sh
+++ tools/download_or_update_problems.sh
@@ -10,6 +10,7 @@ for i in $(seq 1 1000); do
       jq -c . > /tmp/json2
     cp /tmp/json2 problems/problem-$i.json
     cp /tmp/json2 webapp/streamlit/resource/problems/problem-$i.json
+    cp /tmp/json2 webapp/fastapi/resource/problems/problem-$i.json
   else
     echo Failure
     break
