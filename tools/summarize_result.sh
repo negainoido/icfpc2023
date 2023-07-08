68a4809c855d6105e9a7f05104753d630842cd5a
autotaker <autotaker@gmail.com>
2023-07-09 07:14:03 +0900

    add debug options

--- tools/summarize_result.sh
+++ tools/summarize_result.sh
@@ -2,7 +2,7 @@
 
 printf "|problem_id|score|best|comment|\n" > summary.md
 printf "|----------|----:|---:|-------|\n" >> summary.md
-for i in $(seq 1 90); do
+for i in $(seq 1 10); do
   best=$(curl "localhost:8080/api/best_solutions?id=$i" -s | jq -r .score)
   score=$(cat answer-$i.score.txt | sed -e 's/Score: //')
   re='^-?[0-9]+$'
