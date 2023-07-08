a1b56927e6616f8590a424a2e5879965f5c2fda1
autotaker <autotaker@gmail.com>
2023-07-08 19:07:02 +0900

    improve report readability

--- tools/summarize_result.sh
+++ tools/summarize_result.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 printf "|problem_id|score|best|comment|\n" > summary.md
-printf "|----------|-----|----|-------|\n" >> summary.md
+printf "|----------|----:|---:|-------|\n" >> summary.md
 for i in $(seq 1 55); do
   best=$(curl "localhost:8080/api/best_solutions?id=$i" -s | jq -r .score)
   score=$(cat answer-$i.score.txt | sed -e 's/Score: //')
   re='^-?[0-9]+$'
   if [[ $score =~ $re ]] ; then
     if [[ $score -ge $best ]]; then
-        comment="new best!($(($score * 100 / $best))%%)"
+        comment=$(printf "new best!(%.3e)" $(($score - $best)))
     else
-        comment="$(($score * 100 / $best))%%"
+        comment="$(($score - $best))"
     fi
   else
     comment=$score
     score=""
   fi
-  printf "|$i|$score|$best|$comment|\n" >> summary.md
+  printf "|%s|%'d|%'d|%s|\n" $i "$score" "$best" "$comment" >> summary.md
 done
 jq -Rs '.' summary.md | jq -c '{summary:"Batch Result", text_description:.}' > summary.json
