1dbf54b51768362e6674b0d53f270fee943f4535
cympfh <cympfh@gmail.com>
2023-07-08 15:52:36 +0900

    wip
    wip
    
    wip

--- webapp/svelte/src/routes/+page.svelte
+++ webapp/svelte/src/routes/+page.svelte
@@ -1,27 +1,100 @@
 <script>
     import { onMount } from 'svelte';
-    let wasm;
-    let x = 1;
-    let y = 2;
-    let z;
 
-    onMount(async () => {
-        wasm = await import('wasm-sample');
-        await wasm.default();
-        z = wasm.add(x, y);
-    });
+    let wasm;
+    let problem_id = 1;
+    let solution_id = 1;
+    let records = [];
+    let filteredRecords = [];
+    let solution = null;
 
     function updateAddition() {
         if (!wasm) return; // failed
-        z = wasm.add(x, y);
     }
 
+    /// 良いレコード全部取得
+    function fetchRecords() {
+        fetch('https://icfpc2023.negainoido.com/api/solutions/show')
+            .then(data => data.json())
+            .then(data => {
+                records = data;
+                filterRecords();
+            })
+    }
+
+    /// problem_id ごとの結果を表示する
+    function filterRecords() {
+        if (!records) return;
+        filteredRecords = [];
+        for (let r of records) {
+            if (r[1] === problem_id) {
+                filteredRecords.push(r);
+            }
+        }
+        solution = null;
+    }
+
+    function fetchSolution(solution_id) {
+        fetch(`https://icfpc2023.negainoido.com/api/solutions?id=${solution_id}`)
+            .then(response => response.json())
+            .then(response => {
+                if (response['message'] === 'not found') {
+                    alert('not found');
+                    solution = null;
+                    return;
+                }
+                let contents = response['contents'];
+                console.log(contents);
+                solution = JSON.parse(contents);
+                draw();
+            });
+    }
+
+    function draw() {
+        let canvas = document.getElementById('c').getContext('2d');
+        let width = 1200;
+        let height = 800;
+        canvas.clearRect(0, 0, width, height);
+    }
+
+    onMount(async () => {
+        fetchRecords();
+        wasm = await import('wasm-sample');
+        await wasm.default();
+    });
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<label>problem_id</label>
+<input type='number' bind:value={problem_id} on:change={filterRecords} />
+
 <div>
-    <input type="number" bind:value={x} on:change={updateAddition} /> +
-    <input type="number" bind:value={y} on:change={updateAddition} /> =
-    <input type="number" bind:value={z} disabled />
+<p>{filteredRecords.length} records</p>
+{#if filteredRecords.length > 0}
+    <table>
+        <tr>
+                <th>id</th>
+                <th>problem_id</th>
+                <th>submission_id</th>
+                <th>solver</th>
+                <th>status</th>
+                <th>score</th>
+                <th>ts</th>
+        </tr>
+        {#each filteredRecords as r}
+            <tr>
+                <td><button on:click={fetchSolution(r[0])}>{r[0]}</button></td>
+                <td>{r[1]}</td>
+                <td>{r[2]}</td>
+                <td>{r[3]}</td>
+                <td>{r[4]}</td>
+                <td>{r[5]}</td>
+                <td>{r[6]}</td>
+            </tr>
+        {/each}
+    </table>
+{/if}
+</div>
+
+<div>
+  <canvas id="c" width="1200" height="800" style='border:1px black solid' />
 </div>
