ea886b6b9aa431f5abe6ae51fba0b9b9f48a90ac
Takuto Ikuta <atetubou@gmail.com>
2023-07-09 17:37:58 +0900

    update submit.hs

--- tools/submit.sh
+++ tools/submit.sh
@@ -7,8 +7,12 @@ set -euxo pipefail
 # 自分たちのサーバーの提出用エンドポイントを叩きます。ICFPC2023のサーバーにも提出されます。
 # `gcloud auth login` しておく必要があります
 
+# あと↓のコマンドも走らせてください。
+# $ gcloud beta run services proxy fastapi-iam-auth --port=8080 --region asia-northeast1
+
 SOLVER=${SOLVER:-default}
 ANSWER_FILE=$(readlink -f "$ANSWER_FILE")
 
 TOKEN=$(gcloud auth print-access-token)
-curl -X POST -H "Authorization: Bearer ${TOKEN}" -F file=@"${ANSWER_FILE}" "https://icfpc2023.negainoido.com/api/solutions/submit?id=${PROBLEM_ID}&solver=${SOLVER}"
+# curl -X POST -H "Authorization: Bearer ${TOKEN}" -F file=@"${ANSWER_FILE}" "https://icfpc2023.negainoido.com/api/solutions/submit?id=${PROBLEM_ID}&solver=${SOLVER}"
+curl -X POST -H "Authorization: Bearer ${TOKEN}" -F file=@"${ANSWER_FILE}" "http://localhost:8080/api/solutions/submit?id=${PROBLEM_ID}&solver=${SOLVER}"
