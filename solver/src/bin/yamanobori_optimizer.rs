f483e4a1f330c89dada0667bb5d3d7e3d5941942
Takuto Ikuta <atetubou@gmail.com>
2023-07-10 12:48:25 +0900

    git squash commit for yamanobori_optimizer. (#214)
    845cde7f2cdccd8e34020f005edd073c1763265c
    git squash commit for yamanobori_optimizer.
    
    d42a16659936ae6b4bb73d0b79116b1da6a8f80a
    yamanobori
    
    77338aa55a1f0f33b8108cb844d9901ac4216cd2
    add yamanobori optimizer
    
    0616135548b627300506b0f8d55bfdf5e65e513b
    solution

---
+++ solver/src/bin/yamanobori_optimizer.rs
@@ -0,0 +1,59 @@
+use clap::Parser;
+use rand::Rng;
+
+use solver::problem::*;
+use solver::solver_util::*;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long)]
+    solution: String,
+
+    #[arg(short, long)]
+    output: String,
+
+    #[arg(short, long, default_value_t = 30.0)]
+    timeout: f64,
+
+    #[arg(short, long)]
+    rand_seed: Option<u128>,
+}
+
+fn main() {
+    let args = Args::parse();
+    let input_str = std::fs::read_to_string(args.input.clone()).unwrap();
+    let input: Input = serde_json::from_str(&input_str).unwrap();
+
+    let solution_str = std::fs::read_to_string(args.solution.clone()).unwrap();
+    let mut solution: Solution = serde_json::from_str(&solution_str).unwrap();
+
+    let mut rng = rand::thread_rng();
+
+    let seed = args.rand_seed.unwrap_or(rng.gen::<u128>());
+    eprintln!("rand seed: {}", seed);
+
+    let mut best_score = input.score_fast(&solution, false).unwrap();
+    let volumes = solution.volumes.clone();
+
+    let placements = yamanobori(
+        &input,
+        &mut best_score,
+        &mut solution.placements,
+        &solution
+            .volumes
+            .unwrap_or(vec![10.0; input.musicians.len()]),
+        args.timeout,
+    );
+
+    let solution = Solution {
+        placements,
+        volumes,
+    };
+
+    std::fs::write(args.output, serde_json::to_string(&solution).unwrap()).unwrap()
+}
