9a7ec9f81c67f076bf445c812307eddcba44379d
Takuto Ikuta <atetubou@gmail.com>
2023-07-10 11:17:16 +0900

    extract timeout (#206)

--- solver/src/lib.rs
+++ solver/src/lib.rs
@@ -199,6 +199,20 @@ impl<'a> PlacementGenerator<'a> {
     }
 }
 
+pub fn get_time() -> f64 {
+    static mut STIME: f64 = -1.0;
+    let t = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap();
+    let ms = t.as_secs() as f64 + t.subsec_nanos() as f64 * 1e-9;
+    unsafe {
+        if STIME < 0.0 {
+            STIME = ms;
+        }
+        ms - STIME
+    }
+}
+
 fn get_id(input: &str) -> anyhow::Result<i32> {
     let path = std::path::Path::new(&input);
     let base = path.file_stem().context("no file_stem?")?;
